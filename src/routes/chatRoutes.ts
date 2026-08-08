4ad730eae78ef223aae4df120cbf4e97acc9ffb0
Devansh Maheshwari <devanshmaheshwari2310@gmail.com>
2026-08-08 10:52:28 +0530

    Fix import path for authMiddleware

--- src/routes/chatRoutes.ts
+++ src/routes/chatRoutes.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { chat, getCropAdvice, analyzePest, textToSpeech } from '../controllers/chatController';
-import { authMiddleware } from '../middleware/authMiddleware';
+import { authMiddleware } from '../middlewares/authMiddleware';
 
 const router = express.Router();
 
