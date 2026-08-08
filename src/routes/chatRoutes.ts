52fa03763422cc7225fd38ac63333fb4cce9dd6d
Devansh Maheshwari <devanshmaheshwari2310@gmail.com>
2026-08-08 10:45:50 +0530

    Replace auth middleware import with authMiddleware

--- src/routes/chatRoutes.ts
+++ src/routes/chatRoutes.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { chat, getCropAdvice, analyzePest, textToSpeech } from '../controllers/chatController';
-import { auth } from '../middleware/auth';
+import { authMiddleware } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
